44ae6c33832cc794dd5db074cbf2fdfc52806f4f
pjs0418 <tony180322@gmail.com>
2021-10-06 19:33:19 +0900

    fix: Add validation

--- multi-table.sh
+++ multi-table.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 num1=$1
 num2=$2
+validation="^[1-9]+$"
+
+if ! [[ $num1 =~ $validation ]] || ! [[ $num2 =~ $validation ]]
+then
+	echo "Your input is wrong. Please set input by integer and bigger than 0."
+	exit 1
+fi
 
 for i in $(seq 1 $num1)
 do
