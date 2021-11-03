bba53bda6c46b4ec60e0602a8fb29f49d89ee2f1
pjs0418 <tony180322@gmail.com>
2021-11-03 15:21:34 +0900

    chore: apply gitignore

--- 06/pxt4/jbd3/.jbd3.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/junsu/Linux-Assignment/06/pxt4/jbd3/jbd3.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/junsu/Linux-Assignment/06/pxt4/jbd3/jbd3.ko /home/junsu/Linux-Assignment/06/pxt4/jbd3/jbd3.o /home/junsu/Linux-Assignment/06/pxt4/jbd3/jbd3.mod.o;  true
