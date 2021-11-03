8aaccf3d87aa814db3b9fb16be7c21cce7a37972
pjs0418 <tony180322@gmail.com>
2021-11-03 14:59:46 +0900

    complete: assignment 06(Kernel Module)

---
+++ 06/pxt4/.pxt4.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/junsu/Linux-Assignment/06/pxt4/pxt4.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/junsu/Linux-Assignment/06/pxt4/pxt4.ko /home/junsu/Linux-Assignment/06/pxt4/pxt4.o /home/junsu/Linux-Assignment/06/pxt4/pxt4.mod.o;  true
